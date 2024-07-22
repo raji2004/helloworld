476a414cbd3721e7d9b12b54b21595f44646410f
raji <kingraj1344@gmail.com>
2024-07-22 09:40:27 +0100

    feat: Add ArithmeticDemo, ArrayDemo, and BreakWithLabelDemo classes

---
+++ src/ArithmeticDemo.java
@@ -0,0 +1,25 @@
+class ArithmeticDemo {
+
+    public static void main (String[] args){
+        java.util.ArrayList<String> futureMonths =
+            new java.util.ArrayList<String>();
+         int result = 1;
+          result +=  2; // result is now 3
+         System.out.println(result);
+
+        result -= 1; // result is now 2
+         System.out.println(result);
+
+         result *=  2; // result is now 4
+         System.out.println(result);
+
+         result /= 2; // result is now 2
+         System.out.println(result);
+
+         result += 8; // result is now 10
+         System.out.println(result);
+         result %=7; // result is now 3
+         System.out.println(result);
+    }
+}
+
---
+++ src/ArrayDemo.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class ArrayDemo {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = x++; // y gets the value of x (5), then x becomes 6
+        System.out.println("x = " + x + ", y = " + y);
+        String[] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+
+        String[] copyTo = new String[7];
+        // System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        int i = 0;
+        for (String coffee : copyFrom) {
+            if (i < 7) {
+                copyTo[i] = coffee;
+                i++;
+            } else {
+                break;
+            }
+            // System.out.println(Arrays.toString(copyTo));
+        }
+    }
+}
+class Array extends ArrayDemo {
+    // public static void main(String[] args) {
+    //     int[] numbers = { 1, 2, 3, 4, 5 };
+    //     int[] numbersCopy = Arrays.copyOf(numbers, 3);
+    //     System.out.println(Arrays.toString(numbersCopy));
+    // }
+}
